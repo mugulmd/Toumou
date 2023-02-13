5d9d4e717a501cad5ba6559bc69ea6ac53b0f975
Loïc Vital <mugulmotion@gmail.com>
2023-02-13 21:54:45 +0100

    [python] added keyword arguments

--- src/bindings/toumou_python.cpp
+++ src/bindings/toumou_python.cpp
@@ -64,14 +64,20 @@ PYBIND11_MODULE(toumou, m)
 	// Camera
 
 	py::class_<Camera, std::shared_ptr<Camera>>(m, "Camera")
-		.def(PYTMKS(Camera, float, float, float, float))
+		.def(PYTMKS(Camera, float, float, float, float),
+			py::arg("sensor_width"),
+			py::arg("field_of_view"),
+			py::arg("z_near"),
+			py::arg("z_far"))
 		.def("move_to", &Camera::move_to)
 		.def("rotate_to", &Camera::rotate_to);
 
 	// Image
 
 	py::class_<Image<Color>>(m, "Image")
-		.def(py::init<int, int>())
+		.def(py::init<int, int>(),
+			py::arg("width"),
+			py::arg("height"))
 		.def("at", &Image<Color>::at)
 		.def("set", &Image<Color>::set);
 
@@ -82,16 +88,26 @@ PYBIND11_MODULE(toumou, m)
 		.def_readwrite("color", &Light::color);
 
 	py::class_<PointLight, std::shared_ptr<PointLight>, Light>(m, "PointLight")
-		.def(PYTMKS(PointLight, const Color&, float, const Vec3&));
+		.def(PYTMKS(PointLight, const Color&, float, const Vec3&),
+			py::arg("color"),
+			py::arg("brightness"),
+			py::arg("location"));
 
 	py::class_<DirectionalLight, std::shared_ptr<DirectionalLight>, Light>(m, "DirectionalLight")
-		.def(PYTMKS(DirectionalLight, const Color&, float, const Vec3&));
+		.def(PYTMKS(DirectionalLight, const Color&, float, const Vec3&),
+			py::arg("color"),
+			py::arg("brightness"),
+			py::arg("direction"));
 
 	py::class_<EnvironmentLight, std::shared_ptr<EnvironmentLight>>(m, "EnvironmentLight")
-		.def(PYTMKS(EnvironmentLight, const Color&, float));
+		.def(PYTMKS(EnvironmentLight, const Color&, float),
+			py::arg("color"),
+			py::arg("brightness"));
 
 	py::class_<PanoramaLight, std::shared_ptr<PanoramaLight>, EnvironmentLight>(m, "PanoramaLight")
-		.def(PYTMKS(PanoramaLight, const Image<Color>&, float));
+		.def(PYTMKS(PanoramaLight, const Image<Color>&, float),
+			py::arg("image"),
+			py::arg("brightness"));
 
 	// Field
 
@@ -99,35 +115,52 @@ PYBIND11_MODULE(toumou, m)
 		.def("value", &Field::value);
 
 	py::class_<Constant, std::shared_ptr<Constant>, Field>(m, "Constant")
-		.def(PYTMKS(Constant, float));
+		.def(PYTMKS(Constant, float),
+			py::arg("value"));
 
 	py::class_<Fusion, std::shared_ptr<Fusion>, Field>(m, "Fusion")
 		.def(PYTMKS(Fusion))
-		.def("add", &Fusion::add);
+		.def("add", &Fusion::add,
+			py::arg("field"),
+			py::arg("coef"));
 
 	py::class_<Dist2ToPoint, std::shared_ptr<Dist2ToPoint>, Field>(m, "Dist2ToPoint")
-		.def(PYTMKS(Dist2ToPoint, const Vec3&));
+		.def(PYTMKS(Dist2ToPoint, const Vec3&),
+			py::arg("center"));
 
 	py::class_<Dist2ToLine, std::shared_ptr<Dist2ToLine>, Field>(m, "Dist2ToLine")
-		.def(PYTMKS(Dist2ToLine, const Vec3&, const Vec3&));
+		.def(PYTMKS(Dist2ToLine, const Vec3&, const Vec3&),
+			py::arg("origin"),
+			py::arg("direction"));
 
 	py::class_<SignedDistToPlane, std::shared_ptr<SignedDistToPlane>, Field>(m, "SignedDistToPlane")
-		.def(PYTMKS(SignedDistToPlane, const Vec3&, const Vec3&));
+		.def(PYTMKS(SignedDistToPlane, const Vec3&, const Vec3&),
+			py::arg("origin"),
+			py::arg("normal"));
 
 	py::class_<Remapping, std::shared_ptr<Remapping>, Field>(m, "Remapping")
 		.def("remap", &Remapping::remap);
 
 	py::class_<Inverse, std::shared_ptr<Inverse>, Remapping>(m, "Inverse")
-		.def(PYTMKS(Inverse, std::shared_ptr<Field>, float));
+		.def(PYTMKS(Inverse, std::shared_ptr<Field>, float),
+			py::arg("field"),
+			py::arg("radius"));
 
 	py::class_<Exponential, std::shared_ptr<Exponential>, Remapping>(m, "Exponential")
-		.def(PYTMKS(Exponential, std::shared_ptr<Field>, float));
+		.def(PYTMKS(Exponential, std::shared_ptr<Field>, float),
+			py::arg("field"),
+			py::arg("factor"));
 
 	py::class_<Smoothstep, std::shared_ptr<Smoothstep>, Remapping>(m, "Smoothstep")
-		.def(PYTMKS(Smoothstep, std::shared_ptr<Field>, float, float));
+		.def(PYTMKS(Smoothstep, std::shared_ptr<Field>, float, float),
+			py::arg("field"),
+			py::arg("in_min"),
+			py::arg("in_max"));
 
 	py::class_<CellNoise, std::shared_ptr<CellNoise>, Field>(m, "CellNoise")
-		.def(PYTMKS(CellNoise, float, int));
+		.def(PYTMKS(CellNoise, float, int),
+			py::arg("grid_size"),
+			py::arg("grid_resolution"));
 
 	// Material
 
@@ -155,16 +188,24 @@ PYBIND11_MODULE(toumou, m)
 		.def_readwrite("material", &Surface::material);
 
 	py::class_<Sphere, std::shared_ptr<Sphere>, Surface>(m, "Sphere")
-		.def(PYTMKS(Sphere, const Vec3&, float));
+		.def(PYTMKS(Sphere, const Vec3&, float),
+			py::arg("center"),
+			py::arg("radius"));
 
 	py::class_<Plane, std::shared_ptr<Plane>, Surface>(m, "Plane")
-		.def(PYTMKS(Plane, const Vec3&, const Vec3&));
+		.def(PYTMKS(Plane, const Vec3&, const Vec3&),
+			py::arg("origin"),
+			py::arg("normal"));
 
 	py::class_<Tube, std::shared_ptr<Tube>, Surface>(m, "Tube")
-		.def(PYTMKS(Tube, const Vec3&, const Vec3&, float));
+		.def(PYTMKS(Tube, const Vec3&, const Vec3&, float),
+			py::arg("origin"),
+			py::arg("direction"),
+			py::arg("radius"));
 
 	py::class_<ImplicitSurface, std::shared_ptr<ImplicitSurface>, Surface>(m, "ImplicitSurface")
-		.def(PYTMKS(ImplicitSurface, std::shared_ptr<Field>))
+		.def(PYTMKS(ImplicitSurface, std::shared_ptr<Field>),
+			py::arg("field"))
 		.def_readwrite("root_estimator", &ImplicitSurface::root_estimator);
 
 	// Scene
@@ -184,7 +225,9 @@ PYBIND11_MODULE(toumou, m)
 		.def_readwrite("max_bounce", &RayTracer::max_bounce)
 		.def_readwrite("rays_per_bounce", &RayTracer::rays_per_bounce)
 		.def_readwrite("env_sampling", &RayTracer::env_sampling)
-		.def("render", &RayTracer::render);
+		.def("render", &RayTracer::render,
+			py::arg("scene"),
+			py::arg("progress_callback"));
 
 	// IO
 
